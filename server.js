72891e5ce2b4ebf335fadd291c01fc4023dbc90f
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-02-02 18:04:09 -0700

    change port

--- server.js
+++ server.js
@@ -32,4 +32,4 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 80)
