729d3fd74693e34b8ab999236b6f703bf962dbbc
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-10-30 13:36:44 -0700

    include dotenv in daily-message sender

--- daily-message.js
+++ daily-message.js
@@ -1,7 +1,6 @@
 // Imports
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
+require("dotenv").config();
+
 const cron = require("node-cron");
 const sendDailyDogMessage = require("./api/twilio-multisend-api.js");
 const mongoose = require("mongoose");
