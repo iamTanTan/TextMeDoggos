e3db06bdfb56d2d1ca7fca02e2262c308a8609c8
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2022-10-29 22:51:15 -0700

    add cron job

--- daily-message.js
+++ daily-message.js
@@ -1,29 +1,32 @@
-// Imports 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
+// Imports
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
 }
-const sendDailyDogMessage = require('./api/twilio-multisend-api.js')
+const cron = require("node-cron");
+const sendDailyDogMessage = require("./api/twilio-multisend-api.js");
+const mongoose = require("mongoose");
 
-// Establish connection to MongoDB Atlas
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose
+    .connect(process.env.DATABASE_URL, {
         useNewUrlParser: true,
         useCreateIndex: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     })
-    .then(() => console.log('Database Connected'))
-    .catch(err => console.log(err));
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(err));
 
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-// This code sends messages to all current users
-async function sendAll() {
-    await sendDailyDogMessage()
-}
-
-sendAll()
-.then(
-    console.log('Messaging successful.')
-    )
+cron.schedule(
+    "0 8 * * *",
+    async () => {
+        await sendDailyDogMessage();
+        console.log("daily message sent");
+    },
+    {
+        scheduled: true,
+        timezone: "America/Sao_Paulo",
+    }
+);
